498a70c38a3458302020d3413ee0c7f458a0c2b6
Landtree <delandtree@gmail.com>
2025-11-21 14:35:16 -0800

    test code

---
+++ Energy-Transmission/Tests/mag_test/src/define.h
@@ -0,0 +1,14 @@
+#include <FastLED.h>
+#include <Timer.h>
+
+#define ledPin 3
+#define numLeds 3
+
+#define sen1 A1
+#define sen2 A2
+#define sen3 A3
+
+MoToTimer readTime, outputTime;
+
+CRGB senLight[3];
+
